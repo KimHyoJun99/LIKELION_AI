0be6fb9ed5d8679bbcb989c5b1cb1c0f7a81b871
magichjk <magichjk@khu.ac.kr>
2023-10-10 15:42:47 +0900

    solution code review

--- Python_TIL/과제/baseball/baseballgame_sol.py
+++ Python_TIL/과제/baseball/baseballgame_sol.py
@@ -2,11 +2,13 @@ import random
 
 class BaseballGame:
     def selectNumbers(self):
-        samples = [n for n in range(1, 10)]
-        random.shuffle(samples)
-        self._data = samples[0:3]
+        samples = [n for n in range(1, 10)]  # 리스트 컴프리헨션
+        random.shuffle(samples)  # 랜덤으로 섞은 후
+        self._data = samples[0:3]  # 리스트의 0~2번째 위치까지 반환
+        
+        # self.data = random.sample([i for i in range(10)], 3)
        
-
+    # 예외처리 메서드
     def verifyinput(self, value):
         if value < 1 or value > 9:
             raise Exception("Wrong number entered. Please, try again")
@@ -14,43 +16,43 @@ class BaseballGame:
     def getNumbers(self):
         print("Enter 3 numbers (1 ~ 9)(input exam:3 6 9):", end="")
         try:
-            s1 = input("")
-            n1 = int( s1[0] )
+            s1 = input("")  # s1 = list(map(int, input().split()))
+            n1 = int( s1[0] )  # 띄어쓰기가 포함되어 있어서 짝수 index만 가져오기
             n2 = int( s1[2] )
             n3 = int( s1[4] )
-            self.verifyinput(n1)
+            self.verifyinput(n1)  # 예외 처리 메서드
             self.verifyinput(n2)
             self.verifyinput(n3)
         except Exception as ex:
             print("error",ex)
-            return list()
+            return list()  # except에 걸리면 빈 리스트 반환
         else:
-            return [n1, n2, n3]
+            return [n1, n2, n3]  # 입력 리스트 반환
 
     def judge(self, numbers):
         strikes = 0
         balls = 0
         for i in range(3):
-            if self._data[i] == numbers[i]:
+            if self._data[i] == numbers[i]:  # 위치와 숫자가 같으면 strikes 증가
                 strikes += 1
-            elif numbers[i] in self._data:
+            elif numbers[i] in self._data:  # 포함만 되어 있으면 balls 증가
                 balls += 1
         return strikes, balls
 
     def playball(self):
-        self.selectNumbers()
+        self.selectNumbers()  # selectNumbers 메서드
         is3Strikes = False
         count = 0
         while is3Strikes == False:
-            numbers = self.getNumbers()
+            numbers = self.getNumbers()  # getNumbers 메서드
             if len(numbers) < 3:
-                continue
-            judgeResult = self.judge(numbers)
-            is3Strikes = (judgeResult[0] == 3)
+                continue  #  입력 받은 숫자가 3보다 적으면 반복문 처음으로
+            judgeResult = self.judge(numbers)  # judge 메서드
+            is3Strikes = (judgeResult[0] == 3)  # return 받은 strike가 3이면 True
             if is3Strikes == False:
                 print("Result: %d stike(s) and %d ball(s)" % judgeResult)
-            count += 1
-        print("You got the game in %d times" % count)
+            count += 1  # 시행 횟수 증가
+        print("You got the game in %d times" % count)  # 반복문이 종료되면 출력
 
     def startGame(self):
         while True:
@@ -62,9 +64,9 @@ class BaseballGame:
             menu = int(input("select Num:"))
             if menu == 2:
                 break
-            self.playball()
+            self.playball()  # playball 메서드
 
 if __name__ == '__main__':
-    game = BaseballGame()
-    game.startGame()
+    game = BaseballGame()  # BaseballGame class
+    game.startGame()  # startGame 메서드 실행
     
